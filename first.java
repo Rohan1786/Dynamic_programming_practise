67539884e79fe5c3f02f06fa0d3496c538e5e73f
Rohan1786 <rp9522203@gmail.com>
2024-06-16 12:15:20 +0530

    little changes did here

--- first.java
+++ first.java
@@ -2,7 +2,18 @@ import java.util.Scanner;
 
 public class first {
 
-    
+    public static int Fibo(int n){
+        if(n==0||n==1){
+            return n;
+        }
+
+       
+
+        return Fibo(n-1)+Fibo(n-2);
+
+
+    }
+
     // here we are applying first dp problem to create optimize code based on fibnanci series problem
     public static int Fibomemories(int n,int[]q){
         if(n==0||n==1){
@@ -12,19 +23,22 @@ public class first {
        if(q[n]!=0){
         return q[n];
        }
+       System.out.println("hello"+n);
 
-       
-
-        int fi=Fibomemories(n-1, q)+Fibomemories(n-2, q);
+       int f1=Fibomemories(n-1, q);
+int f2=Fibomemories(n-2, q);
+        int fi=f1+f2;
         q[n]=fi;
         return fi;
+       
 
 
     }
-    public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        System.out.println("enter no to be in fibonanci values");
 
+    public static void main(String[] args) {
+        
+        System.out.println("enter no to be in fibonanci values");
+        Scanner sc =new Scanner(System.in);
         int n=sc.nextInt();
 
        
